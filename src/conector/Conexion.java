bc0311d175d7432cad64838b926a005a50996a52
antho <antho@Anthony>
2025-07-05 21:48:48 -0500

    creacion de los jframe

---
+++ src/conector/Conexion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class Conexion {
+        Connection con;
+
+    public Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            con = DriverManager.getConnection(
+                    
+             
+             //   "jdbc:mysql://teknosa.c7w2cwc4yz5r.us-east-2.rds.amazonaws.com/teknosa_db","antohny","anthony16");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Error en la conexión: " + e.getMessage());
+            con = null;
+        }
+        return con;
+    }
+    
+}
