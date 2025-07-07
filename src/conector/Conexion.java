e03ed98d9a4d6e255f6b4c42b130304944e04f2a
18313 <18313@DESKTOP-799B94D>
2025-07-07 09:03:10 -0500

    Prueba

--- src/conector/Conexion.java
+++ src/conector/Conexion.java
@@ -11,16 +11,16 @@ public class Conexion {
         Connection con;
 
     public Connection getConnection() {
-        try {
+        /*try {
             Class.forName("com.mysql.cj.jdbc.Driver");  
             con = DriverManager.getConnection(
                     
              
-             //   "jdbc:mysql://teknosa.c7w2cwc4yz5r.us-east-2.rds.amazonaws.com/teknosa_db","antohny","anthony16");
-        } catch (ClassNotFoundException | SQLException e) {
+             "jdbc:mysql://teknosa.c7w2cwc4yz5r.us-east-2.rds.amazonaws.com/teknosa_db","antohny","anthony16");
+        } catch (ClassNotFoundException e) {
             System.err.println("Error en la conexión: " + e.getMessage());
             con = null;
-        }
+        }*/
         return con;
     }
     
